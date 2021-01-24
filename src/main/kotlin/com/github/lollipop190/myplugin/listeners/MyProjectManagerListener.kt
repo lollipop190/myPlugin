99aca9355f01ac146c35789cc456331e22d05e7d
GitHub Action <action@github.com>
2021-01-24 03:04:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/lollipop190/myplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/lollipop190/myplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lollipop190.myplugin.listeners
 
+import com.github.lollipop190.myplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/lollipop190/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lollipop190/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lollipop190.myplugin.services
 
+import com.github.lollipop190.myplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
